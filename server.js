ee7ec2812bcc060603fe732cad5eb6b6d8e85944
Courtney Paasch <paasch.courtney@gmail.com>
2020-09-10 22:24:45 -0400

    add JawsDB

--- server.js
+++ server.js
@@ -12,13 +12,17 @@ app.use(express.static("/public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-let connection = mysql.createConnection({
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "DMJR4kidz!",
   database: "burger_db"
-});
+});};
 
 connection.connect((err) => {
   if (err) throw err;
